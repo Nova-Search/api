3b11ce3bd65f52a2c53a3df93fdaad6493a4a6d6
JoshAtticus <72828296+JoshAtticus@users.noreply.github.com>
2024-10-19 20:59:12 +0800

    chore: use code 204 instead of 404 for no results

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def search(query: str = Query(...)):
 
         results = cursor.fetchall()
         if not results:
-            raise HTTPException(status_code=404, detail="No results found.")
+            raise HTTPException(status_code=204, detail="No results found.")
 
         return [
             {
